fa8e9e02517b6cb28b9c9e0e7b01cfb025f7bacf
whitelightningdev <danielmommsen@hotmail.com>
2025-07-13 22:41:00 +0200

    `Update generateSlotsForDay function to set end time to 6:00 PM instead of 7:00 PM`

--- helper/Helper.js
+++ helper/Helper.js
@@ -1,23 +1,24 @@
 function generateSlotsForDay(date) {
-  const startTime = new Date(date);
-  startTime.setHours(8, 0, 0, 0); // 8:00 AM
-
-  const endTime = new Date(date);
-  endTime.setHours(19, 0, 0, 0); // 7:00 PM (so include 6 PM slot)
-
-  let slots = [];
-  let currentTime = startTime;
-
-  while (currentTime < endTime) {
-    const hours = currentTime.getHours();
-    const minutes = currentTime.getMinutes();
-    const formattedTime = `${hours % 12 || 12}:${minutes === 0 ? '00' : minutes} ${hours < 12 ? 'AM' : 'PM'}`;
-    slots.push(formattedTime);
-
-    currentTime.setHours(currentTime.getHours() + 1); // Increment by 1 hour
+    const startTime = new Date(date);
+    startTime.setHours(8, 0, 0, 0); // 8:00 AM
+  
+    const endTime = new Date(date);
+    endTime.setHours(18, 0, 0, 0); // 6:00 PM
+  
+    let slots = [];
+    let currentTime = startTime;
+  
+    while (currentTime < endTime) {
+      const hours = currentTime.getHours();
+      const minutes = currentTime.getMinutes();
+      const formattedTime = `${hours % 12 || 12}:${minutes === 0 ? '00' : minutes} ${hours < 12 ? 'AM' : 'PM'}`;
+      slots.push(formattedTime);
+  
+      currentTime.setHours(currentTime.getHours() + 1); // Increment by 1 hour
+    }
+  
+    return slots;
   }
-
-  return slots;
-}
-
-module.exports = { generateSlotsForDay };
+  
+  module.exports = { generateSlotsForDay };
+  
